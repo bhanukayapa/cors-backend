74cf6d87f8cd38f0e08b432fd3c0a2c75f640661
bhanukayapa <bhanukayapa@gmail.com>
2019-10-28 15:48:41 +0530

    Further append

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.listen(port, function(){
 });
 
 app.get('/api/cookie',function (req, res) {
-    res.setHeader('Set-Cookie','cdn-token=exp=3-header; domain=.herokuapp.com; path=/; SameSite=none; Secure;');
+    res.setHeader('Set-Cookie','cdn-token=exp=3-header; domain=.herokuapp.com; path=/; SameSite=none;');
     res.json({status:'cooked!'});
 });
 
