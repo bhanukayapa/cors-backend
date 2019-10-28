947dd71ee2e9e01cd186309f5800d455a8e6769f
bhanukayapa <bhanukayapa@gmail.com>
2019-10-28 20:04:57 +0530

    localizing

--- index.js
+++ index.js
@@ -1,26 +1,23 @@
 'use strict';
 
-const https = require('https');
 const express = require('express');
 const cors = require('cors');
-const fs = require('fs');
 
 const app = express();
 
-app.use(cors({credentials: true, origin: 'https://cors-frontend-service.herokuapp.com'}));
+app.use(cors({credentials: true, origin: 'http://client1.local.com'}));
 app.use(express.json());
-app.enable('trust proxy');
 
-var port = process.env.PORT || 8080;
-//var host = 'client2.local.com';
+let port = process.env.PORT || 8080;
+let host = 'client2.local.com';
 
-app.listen(port, function(){
+app.listen(port, host, function(){
     console.log('App is listening on ' + port);
 });
 
 app.get('/api/cookie',function (req, res) {
     //res.setHeader('Set-Cookie','cdn-token=exp3header3; path=/; SameSite=None; Secure;');
-    res.setHeader('Set-Cookie','cdn-token=exp3header4; domain=.herokuapp.com; path=/; SameSite=None; Secure;');
+    res.setHeader('Set-Cookie','cdn-token=exp3header4; domain=.local.com; path=/; SameSite=None; Secure;');
     res.json({status:'cooked!'});
 });
 
