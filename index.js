c3ea90d12f39e15d18b2c85c60d93c40c40f994b
bhanukayapa <bhanukayapa@gmail.com>
2019-10-30 16:09:30 +0530

    localizing

--- index.js
+++ index.js
@@ -5,19 +5,20 @@ const cors = require('cors');
 
 const app = express();
 
-app.use(cors({credentials: true, origin: 'http://client1.local.com'}));
+app.use(cors({credentials: true, origin: 'http://client1.local.com:4200'}));
 app.use(express.json());
 
-let port = process.env.PORT || 8080;
+let port = process.env.PORT || 4200;
 let host = 'client2.local.com';
 
 app.listen(port, host, function(){
-    console.log('App is listening on ' + port);
+    console.log('App is listening on ' + host +':' +port);
 });
 
 app.get('/api/cookie',function (req, res) {
-    //res.setHeader('Set-Cookie','cdn-token=exp3header3; path=/; SameSite=None; Secure;');
-    res.setHeader('Set-Cookie','cdn-token=exp3header4; domain=.local.com; path=/; SameSite=None; Secure;');
+    //res.setHeader('Set-Cookie','cdn-token=exp3header1; domain=.local.com; path=/;');
+    res.setHeader('Set-Cookie','cdn-token=exp3header_same-site-none; domain=.local.com; path=/; SameSite=None;');
+    //res.setHeader('Set-Cookie','cdn-token=exp3header_same-site-none-secure; domain=.local.com; path=/; SameSite=None; Secure;');
     res.json({status:'cooked!'});
 });
 
